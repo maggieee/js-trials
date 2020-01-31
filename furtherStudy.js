371150b2bbd9581cad45af5df8e0c8aaede3ee76
Your Name <you@example.com>
2020-01-30 17:55:36 -0800

    almost done with further r study question 1

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,7 +2,17 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+
+  const result = new Set()
+
+  for (const word of words1Set){
+    if (word in Words1Set) {
+      result.add(word)
+    }
+  }
+  return list(result)
 }
 
 
