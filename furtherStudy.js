5855f9aac156d7b16e42f3ad7ea014bf19cbe115
Your Name <you@example.com>
2020-01-31 13:01:35 -0800

    finished wordsInCommon

--- furtherStudy.js
+++ furtherStudy.js
@@ -7,12 +7,12 @@ function wordsInCommon(words1, words2) {
 
   const result = new Set()
 
-  for (const word of words1Set){
-    if (word in Words1Set) {
+  for (const word of words1Set) {
+    if (words2Set.has(word)) {
       result.add(word)
     }
   }
-  return list(result)
+  return Array.from(result)
 }
 
 
